f14024447b0ff09eaca86d0e0218236585ede98f
Wolkodaf <fedoseevvladimir2946@mail.ru>
2026-03-01 11:29:19 +0300

    fix: eventlet -> gevent

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
@@ -23,16 +23,12 @@ def create_app() -> Flask:
     app = Flask(__name__)
     CORS(app, origins="*")
 
-    socketio.init_app(app, cors_allowed_origins="*", async_mode="eventlet")
+    socketio.init_app(app, cors_allowed_origins="*", async_mode="gevent")
     radio.init_app(socketio)
     firemap.init_app(app)
     firesim.init_app(app)
     firesim.init_socketio(socketio)
     game_logic.init_app(app)
-
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return send_from_directory("firemap/plans", f"пепешнелефтв.png")
 
     return app
 
